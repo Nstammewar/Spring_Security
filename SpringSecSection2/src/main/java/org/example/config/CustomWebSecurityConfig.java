2797546012e306b223d425de0bb3f04f6048b80f
nstammewar <nstammewar@gmail.com>
2023-06-03 21:16:14 +0530

    Multiple User Created using InMemoryUserDetailsManager

--- SpringSecSection2/src/main/java/org/example/config/CustomWebSecurityConfig.java
+++ SpringSecSection2/src/main/java/org/example/config/CustomWebSecurityConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,5 +18,12 @@ public class CustomWebSecurityConfig {
                 requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                         .requestMatchers("/notices", "/contact", "/").permitAll()
         ).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder().username("Nilesh").password("12345").authorities("admin").build();
+        UserDetails user = User.withDefaultPasswordEncoder().username("user").password("12345").authorities("user").build();
+        return new InMemoryUserDetailsManager(admin, user);
     }
 }
