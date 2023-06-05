14cb183871ef95525a9353689f92577f5b59904c
nstammewar <nstammewar@gmail.com>
2023-06-05 21:01:00 +0530

    configuration for CORS policy implemented

--- SpringSecSection2/src/main/java/org/example/config/CustomWebSecurityConfig.java
+++ SpringSecSection2/src/main/java/org/example/config/CustomWebSecurityConfig.java
@@ -8,16 +8,28 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 public class CustomWebSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        return http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((requests) ->
-                        requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                                .requestMatchers("/notices", "/contact", "/", "/register").permitAll()
+      return http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(request -> {
+          CorsConfiguration corsConfiguration = new CorsConfiguration();
+          corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+          corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+          corsConfiguration.setAllowCredentials(true);
+          corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+          corsConfiguration.setMaxAge(3600L);
+          return corsConfiguration;
+
+      }))
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact", "/", "/register").permitAll()
                 ).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults()).build();
     }
 
